1a10a7aef98805fa8bf2a029b29a65ba697b9d25
huolongguo1O <121071167+huolongguo1O@users.noreply.github.com>
2023-11-12 06:50:23 +0000

    commit

--- server/model_chatglm.py
+++ server/model_chatglm.py
@@ -54,6 +54,6 @@ system_info = {"role": "system", "content": "Answer the following questions as b
 
 def chat(text):
     history = [system_info]
-query = "帮我查询股票10111的价格"
-response, history = model.chat(tokenizer, query, history=history)
-print(response)
+    query = "帮我查询股票10111的价格"
+    response, history = model.chat(tokenizer, query, history=history)
+    print(response)
