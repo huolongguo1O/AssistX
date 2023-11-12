e3a67ff8ead476f53a4ea338b69fe120eb39866d
huolongguo1O <121071167+huolongguo1O@users.noreply.github.com>
2023-11-12 07:04:44 +0000

    commit

--- server/api.py
+++ server/api.py
@@ -2,5 +2,5 @@ api_list = json.load(open("api_list.json"))
 def is_valid(key):
     for api in api_list:
         if api["key"] == key:
-            return Treu
+            return True
     return False
