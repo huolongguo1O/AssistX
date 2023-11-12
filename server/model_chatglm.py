01b10f47f4628cbbe4a18c3c30098faf4d69f03d
huolongguo1O <121071167+huolongguo1O@users.noreply.github.com>
2023-11-12 07:00:55 +0000

    commit

--- server/model_chatglm.py
+++ server/model_chatglm.py
@@ -57,4 +57,5 @@ def chat(text, _type, history):
     query = text
     if _type == 0:
         response, history = model.chat(tokenizer, query, history=history)
+    if _type == 1: 
     return response, history
