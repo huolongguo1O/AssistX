fb637dba43e1260dc018bfd3be349fa07d2cb92c
huolongguo1O <121071167+huolongguo1O@users.noreply.github.com>
2023-11-12 06:40:02 +0000

    commit

--- server/main.py
+++ server/main.py
@@ -5,11 +5,9 @@ import model_chatglm
 
 app = FastAPI()
 
-
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
 
 @app.get("/api/{api_key}")
 def read_item(api_key: str, q: Union[str, None] = None):
