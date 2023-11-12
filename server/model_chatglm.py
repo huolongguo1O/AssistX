396b4920e8e5a9fd302e3e8730066c8b6b926e11
huolongguo1O <121071167+huolongguo1O@users.noreply.github.com>
2023-11-12 06:48:43 +0000

    commit

--- server/model_chatglm.py
+++ server/model_chatglm.py
@@ -1,2 +1,19 @@
+import os
+import platform
+from transformers import AutoTokenizer, AutoModel
+
+model_path = "THUDM/chatglm3-6b"
+tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
+model = AutoModel.from_pretrained(model_path, trust_remote_code=True).cuda()
+# 多显卡支持，使用下面两行代替上面一行，将num_gpus改为你实际的显卡数量
+# from utils import load_model_on_gpus
+# model = load_model_on_gpus(model_path, num_gpus=2)
+model = model.eval()
+
+os_name = platform.system()
+clear_command = 'cls' if os_name == 'Windows' else 'clear'
+stop_stream = False
+
+welcome_prompt = "欢迎使用 ChatGLM3-6B 模型，输入内容即可进行对话，clear 清空对话历史，stop 终止程序"
 def chat(text):
     pass
