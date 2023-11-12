6b67353c0b86651a4d53ab6c0faf7b7bb3048962
huolongguo1O <121071167+huolongguo1O@users.noreply.github.com>
2023-11-12 07:06:35 +0000

    commit

--- server/main.py
+++ server/main.py
@@ -15,6 +15,9 @@ class query(BaseModel):
 @app.get("/")
 def read_root():
     return "AssistX API!"
+@app.get("/")
+def read_root():
+    return "AssistX API!"
 
 @app.get("/api/{api_key}")
 def read_item(api_key: str, q: Union[query, None] = None):
