395b327bbd51959cd21da790a137d96cfbd3c11d
huolongguo1O <121071167+huolongguo1O@users.noreply.github.com>
2023-11-12 06:59:34 +0000

    commit

--- server/main.py
+++ server/main.py
@@ -18,5 +18,5 @@ def read_root():
 def read_item(api_key: str, q: Union[query, None] = None):
     if not api.is_valid(api_key):
         return {"status":"error", "info":"Invalid key"}
-    return model_chatglm.chat(q.text, q._type, query.history)
+    return model_chatglm.chat(q.text, q._type, q.history)
     
