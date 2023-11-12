90b476db0396c5de9048960782d2ca3bdee23f64
huolongguo1O <121071167+huolongguo1O@users.noreply.github.com>
2023-11-12 06:38:53 +0000

    commit

--- server/main.py
+++ server/main.py
@@ -11,6 +11,6 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
+@app.get("/spi/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
