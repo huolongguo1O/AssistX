b00cefa30301cd6257c625c55f825a28b31acd40
huolongguo1O <121071167+huolongguo1O@users.noreply.github.com>
2023-11-18 07:31:42 +0000

    commit

--- client/main.py
+++ client/main.py
@@ -6,7 +6,7 @@ import gradio as gr
 # def speechr(msg, audio_input):
 
 def chat(msg,chatbot):
-    response = core.chat_core.chat(msg,chatbot)
+    response = core.chat_core.chat(msg)
     return [(msg,'1')]
 with gr.Blocks() as demo:
     title = gr.Markdown("# AssistX")
