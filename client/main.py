14846068004c6e6bbad0064f606e2ece4f5f8b83
huolongguo1O <121071167+huolongguo1O@users.noreply.github.com>
2023-11-18 07:03:48 +0000

    commit

--- client/main.py
+++ client/main.py
@@ -3,7 +3,7 @@ import core.chat_core
 import gradio as gr
 
 
-# def speechr()
+def speechr()
 
 
 with gr.Blocks() as demo:
@@ -12,6 +12,6 @@ with gr.Blocks() as demo:
     msg = gr.Textbox()
     audio_input = gr.Audio(type="filepath")
     audio_input.stop_recording(fn, inputs = [msg, audio_input], output = [msg])
-    # msg.submit(fn, inputs = [msg], outputs = [chatbot])
+    msg.submit(fn, inputs = [msg], outputs = [chatbot])
 
 demo.launch()
