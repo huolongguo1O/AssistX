b7d38a34d8dcd6dd37d7097c99bcaeeb05d23b86
huolongguo1O <121071167+huolongguo1O@users.noreply.github.com>
2023-11-18 07:52:29 +0000

    commit

--- client/core/chat_core.py
+++ client/core/chat_core.py
@@ -9,7 +9,7 @@ class query(BaseModel):
     _type: int # 0 for chat and 1 for observation
     history: str
     '''
-    requests.posts(
+    requests.post(
         load_config.api()+load_config.key()
-    
+
     )
