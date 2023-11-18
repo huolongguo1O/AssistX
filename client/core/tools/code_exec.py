f3b540ccf6b46b18fb1d27daa8f47ae0af2aeb53
huolongguo1O <121071167+huolongguo1O@users.noreply.github.com>
2023-11-18 09:03:14 +0000

    commit

--- client/core/tools/code_exec.py
+++ client/core/tools/code_exec.py
@@ -4,4 +4,4 @@ def main(code):
     save the code to a random file, and execute
     '''
     filename = '/tmp/' + ''.join(random.sample('abcdefghijklmnopqrstuvwxyz', 10))
-    
+    with open(filename, 'w') as f:
