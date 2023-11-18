114fd7fb7c68dcb4d04a246f2561e9551a5e05d6
huolongguo1O <121071167+huolongguo1O@users.noreply.github.com>
2023-11-18 07:50:11 +0000

    commit

--- client/core/chat_core.py
+++ client/core/chat_core.py
@@ -3,7 +3,12 @@ import json
 import load_config
 history = "None"
 def chat(msg):
-    ''
+    '''
+    class query(BaseModel):
+    text: str
+    _type: int # 0 for chat and 1 for observation
+    history: str
+    '''
     requests.get(
         load_config.api()+load_config.key()
 
