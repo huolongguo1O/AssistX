b4e25137c831e169991b9a3031ffa75096bd4411
huolongguo1O <121071167+huolongguo1O@users.noreply.github.com>
2023-11-18 09:03:44 +0000

    commit

--- client/core/tools/code_exec.py
+++ client/core/tools/code_exec.py
@@ -6,4 +6,4 @@ def main(code):
     filename = '/tmp/' + ''.join(random.sample('abcdefghijklmnopqrstuvwxyz', 10))
     with open(filename, 'w') as f:
         f.write(code)
-    os.popen("python3 ")
+    os.popen("python3 "+filename)
